3edcd1fcb640cb4c98407ad2a228cdbb67cd115a
solta <230930@astanait.edu.kz>
2025-06-02 04:34:23 +0500

    Change greeting text

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, SRE Final Project!"
+    return "Hello, SRE Final Project yeah!"
 
 if __name__ == "__main__":
     app.run(debug=True)
